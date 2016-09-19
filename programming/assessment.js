b66f750bc9c07b66d0b9988127d912334a75dd76
Karina Machado <karinamachado@msn.com>
2016-09-18 21:15:37 -0300

    Update assessment3, final course

--- programming/assessment.js
+++ programming/assessment.js
@@ -1,22 +1,57 @@
 //emptyFunc
 
+var emptyFunc = function(){
+
+
+}
+
+emptyFunc();
+
 
 
 
 
 //myProfile
 
-
-
+myProfile ={};
+myProfile.name = "karina";
+myProfile.age = 39;
+myProfile.knowsJavascript = true;
+myProfile.pet = [dog, cat, bird, parrot]
 
 
 //printProfile
-
+var printProfile = function(myProfile){
+ 
+ for ( var key in myProfile)
+ console.log(key); 
+}
 
 
 
 
 //sumOf
+array com valores
+nomearray+indice+indice a soma seria pelo indice do array
+
+var sumOf = function(arrayValues){
+	
+	var total = 0;
+
+
+for( var i=0; i< arrayValues.length; i++){
+
+	total+=arrayValues[i]
+}
+
+return total;
+
+
+};
+
+var my_array = [1,2,3,4,5];
+
+console.log(sumOf(my_array));
 
 
 
